6c7bac6910582f6cf38c13551c4b994ab89bba29
Sasha <sasha.vessa@gmail.com>
2025-08-07 04:03:36 -0300

    first commit

---
+++ src/test/java/_ix/jake/CadastroDeUsuarios/CadastroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package _ix.jake.CadastroDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
