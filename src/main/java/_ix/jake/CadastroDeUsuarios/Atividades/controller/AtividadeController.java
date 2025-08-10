9a8956bba64e4b96b9b300bb5ee0270823051491
Sasha <sasha.vessa@gmail.com>
2025-08-10 11:05:45 -0300

    feat: Atualizar os controller com mock de dados

--- src/main/java/_ix/jake/CadastroDeUsuarios/Atividades/controller/AtividadeController.java
+++ src/main/java/_ix/jake/CadastroDeUsuarios/Atividades/controller/AtividadeController.java
@@ -1,4 +1,33 @@
 package _ix.jake.CadastroDeUsuarios.Atividades.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/atividades")
 public class AtividadeController {
+
+    @PostMapping("/adicionar-atividade")
+    public String criarAtividade(){
+        return "Atividade Criado";
+    }
+
+    @GetMapping("/lista-de-atividades")
+    public String mostraratividade(){
+        return "Lista de Atividades";
+    }
+
+    @GetMapping("/atividade-id")
+    public String mostrarAtividadeID(){
+        return "Atividade por id";
+    }
+
+    @PutMapping("/atualizar-atividade-id")
+    public String alterarAtividadePorID(){
+        return "Alterar Atividade";
+    }
+
+    @DeleteMapping("/deletar-atividade-id")
+    public String deletarAtividadePorID(){
+        return "Atividade Deletado";
+    }
 }
--- src/main/java/_ix/jake/CadastroDeUsuarios/Usuarios/controller/UsuariosController.java
+++ src/main/java/_ix/jake/CadastroDeUsuarios/Usuarios/controller/UsuariosController.java
@@ -1,16 +1,39 @@
 package _ix.jake.CadastroDeUsuarios.Usuarios.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/usuarios")
 public class UsuariosController {
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem";
+    }
+
+    @PostMapping("/adicionar-usuario")
+    public String criarUsuario(){
+        return "Usuari Criado";
+    }
+
+    @GetMapping("/lista-de-usuarios")
+    public String mostraratividade(){
+        return "Lista de Usuarios";
+    }
+
+    @GetMapping("/usuario-id")
+    public String mostrarAtividadeID(){
+        return "Usuario por id";
+    }
+
+    @PutMapping("/atualizar-usuario-id")
+    public String alterarAtividadePorID(){
+        return "Alterar usuario";
+    }
+
+    @DeleteMapping("/deletar-usuario-id")
+    public String deletarAtividadePorID(){
+        return "usuario Deletado";
     }
 }
