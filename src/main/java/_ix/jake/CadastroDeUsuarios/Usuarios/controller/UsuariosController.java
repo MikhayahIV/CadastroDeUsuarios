43e498e7e9bcb2607d9477dcceabdf407b0cb697
Sasha <sasha.vessa@gmail.com>
2025-10-06 01:08:49 -0300

    doc: Adiciona swagger na minha aplicacao para documentacao das minhas controllers

--- src/main/java/_ix/jake/CadastroDeUsuarios/Usuarios/controller/UsuariosController.java
+++ src/main/java/_ix/jake/CadastroDeUsuarios/Usuarios/controller/UsuariosController.java
@@ -3,6 +3,10 @@ package _ix.jake.CadastroDeUsuarios.Usuarios.controller;
 
 import _ix.jake.CadastroDeUsuarios.Usuarios.DTOs.UsuariosDTO;
 import _ix.jake.CadastroDeUsuarios.Usuarios.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,11 +24,17 @@ public class UsuariosController {
     }
 
     @GetMapping("/boasVindas")
+    @Operation(summary = "Mensagem de boas vindas",description = "essa rota da uma mensagem de boas vindas para quem acessa ela.")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem";
     }
 
     @PostMapping("/adicionar")
+    @Operation(summary = "Cria um novo Usuarios",description = "Rota cria um novo usuarios e insere no banco de dados.")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "201", description = "Usuario criado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro na criacao do Usuario")
+    })
     public ResponseEntity<String> criarUsuario(@RequestBody UsuariosDTO usuario){
         UsuariosDTO novoUsuario = usuarioService.criarUsuario(usuario);
         return  ResponseEntity.status(HttpStatus.CREATED)
@@ -32,12 +42,22 @@ public class UsuariosController {
     }
 
     @GetMapping("/listar")
+    @Operation(summary = "Lista todos os Usuarios", description = "Esta rota retorna uma lista com todos os Usuarios cadastrados no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Aparece a lista de Usuarios"),
+            @ApiResponse(responseCode = "404", description = "Nenhum Usuario encontrado")
+    })
     public ResponseEntity<List<UsuariosDTO>> mostraraUsuarios(){
         List<UsuariosDTO> usuarios = usuarioService.listarUsuarios();
         return ResponseEntity.ok(usuarios);
     }
 
     @GetMapping("/listar/{id}")
+    @Operation(summary = "Listar usuario por ID",description = "Rota lista um usuarios por id.")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado sucesso."),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado")
+    })
     public ResponseEntity<?> listarPorId(@PathVariable Long id){
         UsuariosDTO usuario = usuarioService.usuarioPorId(id);
         if (usuario != null){
@@ -49,7 +69,16 @@ public class UsuariosController {
     }
 
     @PutMapping("/atualizar/{id}")
-    public ResponseEntity<?> alteraraPorId(@PathVariable Long id, @RequestBody UsuariosDTO usuarioAtualizado){
+    @Operation(summary = "Altera usuario por ID",description = "Rota Altera um usuarios por id.")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "200", description = "Usuario alterado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado, nao foi possivel alterar")
+    })
+    public ResponseEntity<?> alteraraPorId(
+            @Parameter(description = "Usuario manda o ID no caminho da requisicao")
+            @PathVariable Long id,
+            @Parameter(description = "Usuario manda os dados do Usuario a ser atualizado no corpo da requisicao")
+            @RequestBody UsuariosDTO usuarioAtualizado){
         UsuariosDTO usuario = usuarioService.atualizarPorId(id,usuarioAtualizado);
         if(usuario !=null){
             return ResponseEntity.ok(usuario);
@@ -60,6 +89,11 @@ public class UsuariosController {
     }
 
     @DeleteMapping("/deletar/{id}")
+    @Operation(summary = "Deleta um Usuario existente", description = "Esta rota deleta um usuario com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado, nao foi possivel deletar")
+    })
     public ResponseEntity<String> deletarPorId(@PathVariable Long id){
         if(usuarioService.usuarioPorId(id) !=null){
             usuarioService.deletarPorId(id);
--- src/main/resources/db/migrations/V3__Remove_TB_ATIVIDADES_COPY_3_2.sql
+++
@@ -1,5 +0,0 @@
--- V3: remove TB_ATIVIDADES_COPY_3_2  que foi criada pelo hibernate para uma alteração
-
-DROP TABLE TB_ATIVIDADES_COPY_3_2;
-
-ALTER TABLE tb_atividades ALTER COLUMN dificuldade VARCHAR(255);
